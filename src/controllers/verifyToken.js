022b6b92a97ff675096094198338eaed237e1bd7
Calajador <44869546+Calajador@users.noreply.github.com>
2019-12-22 20:00:43 +0100

    Update verifyToken.js

--- src/controllers/verifyToken.js
+++ src/controllers/verifyToken.js
@@ -1,24 +1,23 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
- function verifyToken (req, res, next) {
-     // Leer el Token
-   const token = req.headers['x-access-token'];
+exports.auth = function (req, res, next) {
 
-   // No hay token
-   if(!token) {
-       return res.status(401).json({
-           ok: false,
-           mensaje: 'No hay token'
-       });
-   }
+    // Leer el Token
+    const token = req.headers['x-access-token'];
 
-   // Si hay token lo decodificamos extraemos el id 
+    // No hay token
+    if(!token) {
+        return res.status(401).json({
+            ok: false,
+            mensaje: 'No hay token'
+        });
+    };
+
+    // Si hay token lo decodificamos extraemos el id 
    // Luego lo asignamos a la propiedad userId que creamos, y la pasamos por el req
-   const decoded = jwt.verify(token, config.secret);
-   req.userId = decoded.id;
-
-   next();
-}
-
-module.exports = verifyToken;
+    const decoded = jwt.verify(token, config.secret);
+    req.userId = decoded._id;
+  
+    next();
+  };
